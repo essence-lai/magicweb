2256bad535653e131cba63300131262ab6bd1871
essence-lai <essence.alumni.ubc.ca>
2020-08-09 18:17:14 -0700

    filtering

--- src/components/card/CardList.js
+++ src/components/card/CardList.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import CardSummary from './CardSummary';
 import { Link } from 'react-router-dom';
+import _ from 'lodash';
 
-const CardList = ({cards}) => {
+const CardList = ({cards, cmc}) => {
+    console.log(cmc);
+    if (cmc) {
+        const cm = parseInt(cmc);
+        return (
+            <div className="card-list section">
+                <div className="card z-depth-0 card-summary">
+                    { cards && cards.map(card => {
+                        if (_.isNumber(cm)) {
+                            console.log('hello');
+                            if (cm === card.cmc) {
+                                return (
+                                    <Link to={'/card/' + card.id } key={ card.id }>
+                                        <CardSummary card={card}/>
+                                    </Link>
+                                )
+                            }
+                            return null;
+                        }
+                        return (
+                            <Link to={'/card/' + card.id } key={ card.id }>
+                                <CardSummary card={card}/>
+                            </Link>
+                        )
+                    })}
+                </div>
+            </div>
+        )
+    }
     return (
         <div className="card-list section">
             <div className="card z-depth-0 card-summary">
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -5,13 +5,42 @@ import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
 
 class Dashboard extends Component {
+    state = {
+        "cmc": localStorage.getItem('cmc') || null
+    };
+
+    handleChange = (e) => {
+        localStorage.setItem(e.target.id, e.target.value);
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    };
+
+    handleSubnmit = (e) => {
+        localStorage.clear();
+    };
+
     render () {
         const { cards } = this.props;
         return (
             <div className="dashboard container">
                 <div className="row">
-                    <div className="col s12">
-                        <CardList cards={cards}/>
+                    <div className="col s12 m2">
+                        <form onSubmit={this.handleSubnmit}>
+                            <h5 className="grey-text text-darken-3">Filter</h5>
+
+                            <div className="input-field">
+                                <label htmlFor="cmc">CMC</label>
+                                <input type="number" id="cmc" onChange={this.handleChange}/>
+                            </div>
+
+                            <div className="input-field">
+                                <button className="btn pink lighten-1 z-depth-0">Clear</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="col s12 m10">
+                        <CardList cards={cards} cmc={this.state.cmc}/>
                     </div>
                 </div>
             </div>
@@ -29,6 +58,6 @@ const mapStateToProps = (state) => {
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-        { collection: 'cards', orderBy: ['name'] }
+        { collection: 'cards', orderBy: ['color_identity'] }
     ])
 )(Dashboard);
