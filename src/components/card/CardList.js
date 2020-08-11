50b15b2e70f8b2a942f2ead43986965dfa040881
essence-lai <essence.alumni.ubc.ca>
2020-08-10 23:27:21 -0700

    update filter

--- src/components/card/CardList.js
+++ src/components/card/CardList.js
@@ -3,17 +3,77 @@ import CardSummary from './CardSummary';
 import { Link } from 'react-router-dom';
 import _ from 'lodash';
 
-const CardList = ({cards, cmc}) => {
-    console.log(cmc);
-    if (cmc) {
-        const cm = parseInt(cmc);
+const colorsIncluded = ({ card, colors }) => {
+    let isIncluded = 0;
+
+    for (let color of colors) {
+        if(_.includes(card.colors, color)) {
+            isIncluded+= 1;
+        }
+    }
+
+    return isIncluded === colors.length;
+};
+
+const CardList = ({cards, cmc, r, g, b, w, u }) => {
+    const options = { cmc, r, g, b, w, u};
+    let isOptions = false,
+        cm = null,
+        colors = [];
+
+    Object.keys(options).forEach((key, index) => {
+       if(options[key]){
+           isOptions = true;
+           switch(key) {
+               case 'cmc':
+                   cm = parseInt(cmc);
+                   break;
+               case 'r':
+                   colors.push('R');
+                   break;
+               case 'g':
+                   colors.push('G');
+                   break;
+               case 'b':
+                   colors.push('B');
+                   break;
+               case 'w':
+                   colors.push('W');
+                   break;
+               case 'u':
+                   colors.push('U');
+                   break;
+               default:
+                   break;
+           }
+       }
+    });
+    if (isOptions) {
         return (
             <div className="card-list section">
                 <div className="card z-depth-0 card-summary">
                     { cards && cards.map(card => {
-                        if (_.isNumber(cm)) {
-                            console.log('hello');
+                        if (_.isNumber(cm) && colors.length > 0) {
+                            if ((_.isNumber(cm) && cm === card.cmc) && (colors.length > 0 && colorsIncluded({ card, colors }))) {
+                                return (
+                                    <Link to={'/card/' + card.id } key={ card.id }>
+                                        <CardSummary card={card}/>
+                                    </Link>
+                                )
+                            }
+                            return null;
+                        } else if (_.isNumber(cm)) {
                             if (cm === card.cmc) {
+                                return (
+                                    <Link to={'/card/' + card.id } key={ card.id }>
+                                        <CardSummary card={card}/>
+                                    </Link>
+                                )
+                            }
+                            return null;
+
+                        } else if (colors.length > 0 ) {
+                            if (colorsIncluded({ card, colors })) {
                                 return (
                                     <Link to={'/card/' + card.id } key={ card.id }>
                                         <CardSummary card={card}/>
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,20 +3,31 @@ import  CardList from '../card/CardList';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
+import _ from 'lodash';
 
 class Dashboard extends Component {
     state = {
-        "cmc": localStorage.getItem('cmc') || null
+        "cmc": localStorage.getItem('cmc') || null,
+        "r": localStorage.getItem('r') || false,
+        "g": localStorage.getItem('g') || false,
+        "b": localStorage.getItem('b') || false,
+        "w": localStorage.getItem('w') || false,
+        "u": localStorage.getItem('u') || false
     };
 
     handleChange = (e) => {
-        localStorage.setItem(e.target.id, e.target.value);
+        let value = e.target.value;
+
+        if(_.includes(['r', 'g', 'b', 'w', 'u'], e.target.id)){
+            value = document.getElementById(e.target.id).checked;
+        }
+        localStorage.setItem(e.target.id, value);
         this.setState({
-            [e.target.id]: e.target.value
+            [e.target.id]: value
         })
     };
 
-    handleSubnmit = (e) => {
+    handleSubmit = (e) => {
         localStorage.clear();
     };
 
@@ -26,7 +37,7 @@ class Dashboard extends Component {
             <div className="dashboard container">
                 <div className="row">
                     <div className="col s12 m2">
-                        <form onSubmit={this.handleSubnmit}>
+                        <form onSubmit={this.handleSubmit}>
                             <h5 className="grey-text text-darken-3">Filter</h5>
 
                             <div className="input-field">
@@ -34,13 +45,55 @@ class Dashboard extends Component {
                                 <input type="number" id="cmc" onChange={this.handleChange}/>
                             </div>
 
+                            <p>
+                                <label htmlFor="r" className="input-field">
+                                    <input type="checkbox" id="r" onChange={this.handleChange}/>
+                                    <span>Red</span>
+                                </label>
+                            </p>
+
+                            <p>
+                                <label htmlFor="g">
+                                    <input type="checkbox" id="g" onChange={this.handleChange}/>
+                                    <span>Green</span>
+                                </label>
+                            </p>
+
+                            <p>
+                                <label htmlFor="b">
+                                    <input type="checkbox" id="b" onChange={this.handleChange}/>
+                                    <span>Black</span>
+                                </label>
+                            </p>
+
+                            <p>
+                                <label htmlFor="w">
+                                    <input type="checkbox" id="w" onChange={this.handleChange}/>
+                                    <span>White</span>
+                                </label>
+                            </p>
+
+                            <p>
+                                <label htmlFor="u">
+                                    <input type="checkbox" id="u" onChange={this.handleChange}/>
+                                    <span>Blue</span>
+                                </label>
+                            </p>
+
                             <div className="input-field">
                                 <button className="btn pink lighten-1 z-depth-0">Clear</button>
                             </div>
                         </form>
                     </div>
                     <div className="col s12 m10">
-                        <CardList cards={cards} cmc={this.state.cmc}/>
+                        <CardList
+                            cards={cards}
+                            cmc={this.state.cmc}
+                            r={this.state.r}
+                            g={this.state.g}
+                            b={this.state.b}
+                            w={this.state.w}
+                            u={this.state.u}/>
                     </div>
                 </div>
             </div>
@@ -49,7 +102,6 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => {
-    console.log(state);
     return {
         cards: state.firestore.ordered.cards
     }
