eec48d3c70cf4a215dcfe6690066dd517d01fd5e
essence-lai <essence.alumni.ubc.ca>
2020-08-20 20:36:02 -0700

    deck details

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import SignIn from './components/auth/SignIn';
 import CreateCard from './components/card/CreateCard';
 import CreateDeck from './components/deck/CreateDeck';
 import DeckDashboard from './components/deck/DeckDashboard';
+import DeckDetails from './components/deck/DeckDetails';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
                 <Route path='/createcard' component={CreateCard}/>
                 <Route path='/createdeck' component={CreateDeck}/>
                 <Route path='/decks' component={DeckDashboard}/>
+                <Route path='/deck/:id' component={DeckDetails}/>
             </Switch>
           </div>
       </BrowserRouter>
---
+++ src/components/deck/DeckDetails.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
+import { Redirect } from 'react-router-dom';
+import _ from "lodash";
+
+const DeckDetails = (props) => {
+    const { deck, auth } = props;
+    console.log(deck);
+
+    if(!auth.uid) return <Redirect to='/signin'/>;
+
+    if(deck) {
+        return (
+            <div>
+                <div className="container section card-details">
+                    <div className="row">
+                        {
+                            _.get(deck.partnerCommander, 'image_uris') &&
+                                <div className="col s12 m3 push-m3 center">
+                                    {
+                                        _.get(deck.commander, 'image_uris') &&
+                                        <div>
+                                            <h5>Commander</h5>
+
+                                            <img
+                                                style={{ maxHeight: '100%', maxWidth: '100%'}}
+                                                src={ deck.commander.image_uris.border_crop }
+                                                alt={ deck.commander.image_uris.normal }
+                                            />
+                                        </div>
+
+                                    }
+                                </div>
+                        }
+
+                        {
+                            _.get(deck.partnerCommander, 'image_uris') &&
+                            <div className="col s12 m3 push-m3 center">
+                                <div>
+                                    <h5>Partner Commander</h5>
+
+                                    <img
+                                        style={{ maxHeight: '100%', maxWidth: '100%'}}
+                                        src={ deck.partnerCommander.image_uris.border_crop }
+                                        alt={ deck.partnerCommander.image_uris.normal }
+                                    />
+                                </div>
+                            </div>
+                        }
+
+                        {
+                            !_.get(deck.partnerCommander, 'image_uris') &&
+                                <div className="col s12 m3 push-m5 center">
+                                    {
+                                        _.get(deck.commander, 'image_uris') &&
+                                        <div>
+                                            <h5>Commander</h5>
+
+                                            <img
+                                                style={{ maxHeight: '100%', maxWidth: '100%'}}
+                                                src={ deck.commander.image_uris.border_crop }
+                                                alt={ deck.commander.image_uris.normal }
+                                            />
+                                        </div>
+
+                                    }
+                                </div>
+                        }
+
+                        <div className="col s12 m12">
+                            <div className="card z-depth-1 card-summary">
+                                <div className="card-content grey-text text-darken-3">
+                                    <span className="card-title">{ deck.name } - { deck.commander.name } { deck.partnerCommander.name && `/ ${ deck.partnerCommander.name }`}</span>
+
+                                    <div className="card-action">
+                                        <div>
+                                            Created By: { deck.authorFirstName } { deck.authorLastName }
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="container center">
+                <p>Loading Deck...</p>
+            </div>
+        )
+    }
+};
+const mapStateToProps = (state, ownProps) => {
+    console.log(state);
+    const id = ownProps.match.params.id,
+        decks = state.firestore.data.decks,
+        deck = decks ? decks[id] : null;
+    return {
+        deck: deck,
+        auth: state.firebase.auth
+    }
+};
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'decks' }
+    ])
+)(DeckDetails);
