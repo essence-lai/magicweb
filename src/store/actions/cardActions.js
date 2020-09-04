39f5ee59d5e54b9f0e459a6727dbf591553e617f
essence-lai <essence.alumni.ubc.ca>
2020-09-03 21:08:21 -0700

    updated card actions

--- src/store/actions/cardActions.js
+++ src/store/actions/cardActions.js
@@ -6,16 +6,33 @@ export const createCard = (cards) => {
             authorId = getState().firebase.auth.uid;
 
         for (let card of cards ) {
-            firestore.collection('cards').doc(card.id).set({
-                ...card,
-                authorFirstName: profile.firstName,
-                authorLastName: profile.lastName,
-                authorId: authorId,
-                createdAt: new Date()
-            }).then(() => {
-                dispatch({ type: 'CREATE_CARD', card });
+
+            firestore.collection('cards').doc(card.id).get().then(function(doc) {
+                if(doc.exists) {
+                    const { quantity } = doc.data(),
+                        updateQuantity = parseInt(quantity) + card.quantity;
+
+                    firestore.collection('cards').doc(card.id).update({quantity: updateQuantity}).then(() => {
+                        dispatch({type: 'UPDATE_CARD', card});
+                    }).catch((err) => {
+                        dispatch({type: 'UPDATE_CARD_ERROR', err});
+                    });
+                } else {
+                    firestore.collection('cards').doc(card.id).set({
+                        ...card,
+                        authorFirstName: profile.firstName,
+                        authorLastName: profile.lastName,
+                        authorId: authorId,
+                        createdAt: new Date()
+                    }).then(() => {
+                        dispatch({ type: 'CREATE_CARD', card });
+                    }).catch((err) => {
+                        dispatch({ type: 'CREATE_CARD_ERROR', err });
+                    });
+
+                }
             }).catch((err) => {
-                dispatch({ type: 'CREATE_CARD_ERROR', err });
+               dispatch({ type: 'CREATE_CARD_ERROR', err });
             });
         }
     }
